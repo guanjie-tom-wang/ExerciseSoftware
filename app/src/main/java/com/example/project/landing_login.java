1f10b60dfd39ca3bca4cb289a3906a76bb2c611d
Yutao Xu <yt899494@dal.ca>
2020-06-06 19:46:12 -0300

    adjust landing page

--- app/src/main/java/com/example/project/landing_login.java
+++ app/src/main/java/com/example/project/landing_login.java
@@ -2,13 +2,37 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import java.util.regex.Pattern;
 
 public class landing_login extends AppCompatActivity {
-
+    TextView tv;
+    Button btn;
+    String username;
+    String email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_login);
+        tv=findViewById(R.id.welcomeBox);
+        btn=findViewById(R.id.contacts);
+
+        username = getIntent().getExtras().getString("Value1");
+        tv.setText("Welcome "+username+"!");
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i=new Intent(landing_login.this,contacts.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
     }
 }
