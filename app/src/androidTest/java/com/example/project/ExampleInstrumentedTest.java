305bac50e09f2fe7a1bbbbf20ff4ca7da4b4b3c1
Donghao Lyu <dn570152@dal.ca>
2020-06-07 17:21:26 -0300

    Resolve "Write Espresso Tests"

--- app/src/androidTest/java/com/example/project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/project/ExampleInstrumentedTest.java
@@ -1,13 +1,23 @@
 package com.example.project;
 
 import android.content.Context;
+import android.view.View;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matcher;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -15,8 +25,11 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +37,23 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.project", appContext.getPackageName());
     }
+
+    @Test
+    public void testInterfaceElements() {
+        onView(withId(R.id.username)).check(matches(withHint("Username")));
+        onView(withId(R.id.email)).check(matches(withHint("Email")));
+        onView(withId(R.id.button)).check(matches(withText("Sign up")));
+        onView(withId(R.id.age)).check(matches(withHint("Age")));
+        onView(withId(R.id.weight)).check(matches(withHint("Weight")));
+        onView(withId(R.id.height)).check(matches(withHint("Height")));
+        onView(withId(R.id.password)).check(matches(withHint("Password")));
+        onView(withId(R.id.address)).check(matches(withHint("Address")));
+    }
+
+    @Test
+    public void testMainActivity(){
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.errorInfo)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+
 }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void validate(View view){
+        final TextView error = findViewById(R.id.errorInfo);
+
+        error.setVisibility(View.VISIBLE);
+
     }
 }
