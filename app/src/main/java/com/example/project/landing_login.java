185190e7f1ecf900ee03a1acaab172ea63f4dc5c
Yutao Xu <yt899494@dal.ca>
2020-06-11 15:44:30 -0300

    adding comments to landing_login

--- app/src/main/java/com/example/project/landing_login.java
+++ app/src/main/java/com/example/project/landing_login.java
@@ -1,7 +1,6 @@
 package com.example.project;
-
+//import libraries
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -19,14 +18,18 @@ public class landing_login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_login);
 
+        //create a new intent
         Intent intent = getIntent();
         final String user_name = intent.getStringExtra("User_Name");
         tv=findViewById(R.id.welcomeBox);
         btn=findViewById(R.id.contacts);
 
+        //set welcome words
         tv.setText("Welcome "+user_name+"!");
 
         final Intent contact=new Intent(landing_login.this, check_friend_request.class);
+
+        //start a new activity when click button
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
