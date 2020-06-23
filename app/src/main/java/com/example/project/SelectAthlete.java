1a39e009539d8b5da8fdb6167438b1cc3e7634f3
Donghao Lyu <dn570152@dal.ca>
2020-06-22 23:05:56 -0300

    Adding SelectAthlete Activity

---
+++ app/src/main/java/com/example/project/SelectAthlete.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.reflect.Array;
+
+public class SelectAthlete extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_athlete);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.planets_spinner);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(SelectAthlete.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.drop_lists));
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+    }
+
+
+}
