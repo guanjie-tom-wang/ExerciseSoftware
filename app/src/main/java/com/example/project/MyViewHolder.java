96c485e25117cc30fc4d34b7db4585ceef05f1e4
ruiyuan <lee000158>
2020-07-17 07:27:21 -0700

    Make the recycler view clickable

--- app/src/main/java/com/example/project/MyViewHolder.java
+++ app/src/main/java/com/example/project/MyViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -8,9 +9,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class MyViewHolder extends RecyclerView.ViewHolder {
     TextView name;
+    RelativeLayout relative;
 
     public MyViewHolder(@NonNull View itemView) {
         super(itemView);
         name=(TextView) itemView.findViewById(R.id.text_view_name);
+        relative=itemView.findViewById(R.id.L1);
     }
 }
